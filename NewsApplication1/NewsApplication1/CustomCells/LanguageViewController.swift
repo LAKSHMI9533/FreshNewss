6b9a97d2ca8b9d20c4e484e55a75032178ed4a23
Lakshmi Donthamsetti <ldonthamsetti@uworld.in>
2023-08-29 12:34:54 +0530

    languageViewController created

---
+++ NewsApplication1/NewsApplication1/CustomCells/LanguageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LanguageViewController.swift
+//  NewsApplication1
+//
+//  Created by Lakshmi Donthamsetti on 29/08/23.
+//
+
+import UIKit
+
+class LanguageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var a = [String]()
+    }
+
+
+}
